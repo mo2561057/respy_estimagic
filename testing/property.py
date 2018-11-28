d5e823430509dd8bebe5349dbc56a16dd0215e28
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2018-11-28 17:54:47 +0100

    better logging of tests

--- testing/property.py
+++ testing/property.py
@@ -1,12 +1,19 @@
 import random
 import os
 
+import pandas as pd
 import numpy as np
 
 from respy_smm.tests.test_integration import test_1
 from respy_smm.tests.test_integration import test_2
 
-for _ in range(1):
+counts = dict()
+counts['is_success'] = 0
+counts['num_total'] = 0
+
+for _ in range(2):
+
+    counts['num_total'] += 1
 
     seed = random.randrange(1, 100000)
     np.random.seed(seed)
@@ -14,5 +21,16 @@ for _ in range(1):
     test_1()
     os.system('git clean -df')
 
-    test_2()
+    try:
+        test_2()
+        is_success = True
+    except AssertionError:
+        is_success = False
+
+    if is_success:
+        counts['is_success'] += 1
+
+    df = pd.DataFrame.from_dict(counts, orient='index')
+    df.to_string(open('testing.respy_smm.info', 'w'), index=True, justify='justify-all')
+
     os.system('git clean -df')
