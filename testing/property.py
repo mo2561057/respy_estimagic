33068903f56950090a79e5a75b73a75dd3556384
Philipp Eisenhauer <eisenhauer@policy-lab.org>
2018-11-28 19:50:07 +0100

    testing

--- testing/property.py
+++ testing/property.py
@@ -1,19 +1,12 @@
 import random
 import os
 
-import pandas as pd
 import numpy as np
 
 from respy_smm.tests.test_integration import test_1
 from respy_smm.tests.test_integration import test_2
 
-counts = dict()
-counts['is_success'] = 0
-counts['num_total'] = 0
-
-for _ in range(2):
-
-    counts['num_total'] += 1
+while True:
 
     seed = random.randrange(1, 100000)
     np.random.seed(seed)
@@ -21,16 +14,5 @@ for _ in range(2):
     test_1()
     os.system('git clean -df')
 
-    try:
-        test_2()
-        is_success = True
-    except AssertionError:
-        is_success = False
-
-    if is_success:
-        counts['is_success'] += 1
-
-    df = pd.DataFrame.from_dict(counts, orient='index')
-    df.to_string(open('testing.respy_smm.info', 'w'), index=True, justify='justify-all')
-
+    test_2()
     os.system('git clean -df')
