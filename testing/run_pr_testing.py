1f5a26d57b3430ce28be0e2731139f2041e271bf
peisenha <eisenhauer@policy-lab.org>
2019-04-04 17:03:16 +0200

    robust pr test script

--- testing/run_pr_testing.py
+++ testing/run_pr_testing.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
 """This module allows for some more elaborate testing before the a pull request."""
 import subprocess
+import sys
 
 minutes_property_tests = 1
 num_regression_tests = 10
 
 print(' \n ... running {:} robustness tests'.format(num_regression_tests))
-cmd = './regression.py -t {:}'.format(num_regression_tests)
+cmd = sys.executable + ' regression.py -t {:}'.format(num_regression_tests)
 subprocess.check_call(cmd, shell=True)
 
 print(' \n ... running property tests for {:} minutes'.format(minutes_property_tests))
-cmd = './property.py -m {:}'.format(minutes_property_tests)
+cmd = sys.executable + ' property.py -m {:}'.format(minutes_property_tests)
 subprocess.check_call(cmd, shell=True)
 
 print('\n')
